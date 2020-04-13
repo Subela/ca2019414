6acaf4d69894fb6e6544103516c12f4ef12bbbd5
OlindaSousa <olindasuely@gmail.com>
2020-04-13 21:35:55 +0000

    Configurate Tabs

--- mobdev-ionic-ca2019414/src/app/app-routing.module.ts
+++ mobdev-ionic-ca2019414/src/app/app-routing.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
-  },
-  {
-    path: 'services',
-    loadChildren: () => import('./pages/services/services.module').then( m => m.ServicesPageModule)
-  }
-];
+const routes: Routes = [{
+  path: '',
+  loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+}];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules
+    })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
